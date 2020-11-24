45e1862bcac83a47cd04b8f995a26e5f4c50716b
RavinduDanthika <73063945+RavinduDanthika@users.noreply.github.com>
2020-11-24 13:37:49 +0530

    show data added

--- app/src/main/java/lk/dev/exerciseso/posts/models/Post.kt
+++ app/src/main/java/lk/dev/exerciseso/posts/models/Post.kt
@@ -1,3 +1,8 @@
 package lk.dev.exerciseso.posts.models
 
-data class Post(var userId: Int,var id: Int,var title: String,var body: String)
+data class Post(
+        var userId: Int,
+        var id: Int,
+        var title: String,
+        var body: String
+)
